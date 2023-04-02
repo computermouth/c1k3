fdd6cdb018e740b899af4b7543d4c8ef14fc6165
computermouth <computermouth@crunchbangplusplus.org>
2023-04-01 20:30:41 -0500

    Initial commit

---
+++ main.c
@@ -0,0 +1,188 @@
+#include <stdio.h>
+
+#include "textures.h"
+
+void * map_data = NULL;
+void * model_data = NULL;
+// Particles
+void * model_explosion = NULL;
+void * model_blood = NULL;
+void * model_gib = NULL;
+void * model_gib_pieces[] = { NULL} ;
+// Enemies
+void * model_grunt = NULL;
+void * model_enforcer = NULL;
+void * model_ogre = NULL;
+void * model_zombie = NULL;
+void * model_hound = NULL;
+// Map Objects
+void * model_barrel = NULL;
+void * model_torch = NULL;
+// Weapon view models
+void * model_shotgun = NULL;
+void * model_nailgun = NULL;
+void * model_grenadelauncher = NULL;
+// Pickups
+void * model_pickup_nailgun = NULL;
+void * model_pickup_grenadelauncher = NULL;
+void * model_pickup_box = NULL;
+void * model_pickup_grenades = NULL;
+void * model_pickup_key = NULL;
+void * model_door = NULL;
+// Projectiles
+void * model_grenade = NULL;
+void * model_plasma = NULL; // aka. nail
+// Sounds
+void * sfx_enemy_hit = NULL;
+void * sfx_enemy_gib = NULL;
+void * sfx_enemy_hound_attack = NULL;
+void * sfx_no_ammo = NULL;
+void * sfx_hurt = NULL;
+void * sfx_pickup = NULL;
+void * sfx_plasma_shoot = NULL;
+void * sfx_shotgun_shoot = NULL;
+void * sfx_shotgun_reload = NULL;
+void * sfx_nailgun_shoot = NULL;
+void * sfx_nailgun_hit = NULL;
+void * sfx_grenade_shoot = NULL;
+void * sfx_grenade_bounce = NULL;
+void * sfx_grenade_explode = NULL;
+
+// stubs
+void r_init();
+void ttt(void *);
+
+void game_load() {
+	// r_init();
+
+	/*
+	// Create textures
+	ttt(texture_data).map(r_create_texture);
+	
+	// Load map & model containers	
+	map_data = await map_load_container('l');
+	model_data = await model_load_container('m');
+
+	// Create models. Many models share the same geometry just with different
+	// sizes and textures.
+	// 0: generic blob
+	// 1: humanoid
+	// 2: barrel
+	// 3: q logo
+	// 4: hound
+	// 5: box
+	// 6: nailgun
+	// 7: torch
+
+	model_q = model_init(model_data[3]);
+
+	model_explosion = model_init(model_data[0], 0.1,0.1,0.1);
+	model_blood = model_init(model_data[0], 0.1,0.2,0.1);
+	model_gib = model_init(model_data[0], 0.3,0.6,0.3);
+	
+	model_grunt = model_init(model_data[1], 2.5,2.2,2.5);
+	model_enforcer = model_init(model_data[1], 3,2.7,3);
+	model_zombie = model_init(model_data[1], 1.5,2,1.5);
+	model_ogre = model_init(model_data[1], 4,3,4);
+	model_hound = model_init(model_data[4],2.5,2.5,2.5);
+
+	model_barrel = model_init(model_data[2], 2, 2, 2);
+	model_torch = model_init(model_data[7], 0.6,1,0.6);
+
+	model_pickup_nailgun = model_init(model_data[6], 1, 1, 1);
+	model_pickup_grenadelauncher = model_init(model_data[2], 1, 0.5, 0.5);
+	model_pickup_box = model_init(model_data[5], 0.7, 0.7, 0.7);
+	model_pickup_grenades = model_init(model_data[5], 0.5, 1, 0.5);
+	model_pickup_key = model_init(model_data[5], 0.1, 0.7, 0.1);
+
+	model_door = model_init(model_data[5], 5, 5, 0.5);
+	
+	model_shotgun = model_init(model_data[2], 1,0.2,0.2);
+	model_grenadelauncher = model_init(model_data[2], 0.7,0.4,0.4);
+	model_nailgun = model_init(model_data[6], 0.7,0.7,0.7);
+
+	model_grenade = model_init(model_data[2], 0.3,0.3,0.3);
+	model_nail = model_init(model_data[2], 0.5,0.1,0.1);
+
+	// Take some parts from the grunt model and build individual giblet models
+	// from it. Arms and legs and stuff...
+	for (let i = 0; i < 204; i+=34) {
+		let m = model_init(model_data[1], 2,1,2);
+		m.f[0] += i;
+		m.nv = 34;
+		model_gib_pieces.push(m);
+	}
+
+
+	r_submit_buffer();
+	requestAnimationFrame(run_frame);
+
+	f.onclick = () => g.requestFullscreen();
+	g.onclick = () => {
+		g.onclick = () => c.requestPointerLock();
+		g.onclick();
+		
+		audio_init();
+
+		// Generate sounds
+		sfx_enemy_hit = audio_create_sound(135, [8,0,0,1,148,1,3,5,0,0,139,1,0,2653,0,2193,255,2,639,119,2,23,0,0,0,0,0,0,0]);
+		sfx_enemy_gib = audio_create_sound(140, [7,0,0,1,148,1,7,5,0,1,139,1,0,4611,789,15986,195,2,849,119,3,60,0,0,0,1,10,176,1]);
+		sfx_enemy_hound_attack = audio_create_sound(132, [8,0,0,1,192,1,8,0,0,1,120,1,0,5614,0,20400,192,1,329,252,1,55,0,0,1,1,8,192,3]);
+
+		sfx_no_ammo = audio_create_sound(120, [8,0,0,0,96,1,8,0,0,0,0,0,255,0,0,1075,232,1,2132,255,0,0,0,0,0,0,0,0,0]);
+		sfx_hurt = audio_create_sound(135, [7,3,140,1,232,3,8,0,9,1,139,3,0,4611,1403,34215,256,4,1316,255,0,0,0,1,0,1,7,255,0]);
+		sfx_pickup = audio_create_sound(140, [7,0,0,1,187,3,8,0,0,1,204,3,0,4298,927,1403,255,0,0,0,3,35,0,0,0,0,0,0,0]);
+
+		sfx_plasma_shoot = audio_create_sound(135, [8,0,0,1,147,1,6,0,0,1,159,1,0,197,1234,21759,232,2,2902,255,2,53,0,0,0,0,0,0,0]);
+
+		sfx_shotgun_shoot = audio_create_sound(135, [7,3,0,1,255,1,6,0,0,1,255,1,112,548,1979,11601,255,2,2902,176,2,77,0,0,1,0,10,255,1]);
+		sfx_shotgun_reload = audio_create_sound(125, [9,0,0,1,131,1,0,0,0,0,0,3,255,137,22,1776,255,2,4498,176,2,36,2,84,0,0,3,96,0]);
+
+		sfx_nailgun_shoot = audio_create_sound(130, [7,0,0,1,132,1,8,4,0,1,132,2,162,0,0,8339,232,2,2844,195,2,40,0,0,0,0,0,0,0]);
+		sfx_nailgun_hit = audio_create_sound(135, [8,0,0,1,148,1,0,0,0,0,0,1,255,0,0,2193,128,2,6982,119,2,23,0,0,0,0,0,0,0]);
+
+		sfx_grenade_shoot = audio_create_sound(127, [8,0,0,1,171,1,9,3,0,1,84,3,96,2653,0,13163,159,2,3206,255,2,64,0,0,0,1,9,226,0]);
+		sfx_grenade_bounce = audio_create_sound(168, [7,0,124,0,128,0,8,5,127,0,128,0,125,88,0,2193,125,1,1238,240,1,91,3,47,0,0,0,0,0]);
+		sfx_grenade_explode = audio_create_sound(135, [8,0,0,1,195,1,6,0,0,1,127,1,255,197,1234,21759,232,2,1052,255,4,73,3,25,1,0,10,227,1]);
+
+
+		audio_play(audio_create_song(...music_data), 1, 1);
+		game_init(0);
+		run_frame = game_run;
+		
+	*/
+};
+
+void run_frame(int time_now) {
+	/*
+	r_prepare_frame();
+
+	r_draw(
+		vec3(0,0,0), 0, 0, 1, 
+		model_q.f[0], model_q.f[0], 0,
+		model_q.nv
+	);
+	r_push_light(
+		vec3(Math.sin(time_now*0.00033)*200, 100, -100),
+		10, 255,192,32
+	);
+	r_push_light(
+		vec3_rotate_y(vec3(0, 0, 100),time_now*0.00063),
+		10, 32,64,255
+	);
+	r_push_light(
+		vec3_rotate_y(vec3(100, 0, 0),time_now*0.00053),
+		10, 196,128,255
+	);
+	
+	r_end_frame();
+	requestAnimationFrame(run_frame);
+	*/
+};
+
+int main() {
+    // game_load();
+    
+    printf("%x\n", textures[30].data[2840]);
+    
+}
---
+++ textures.c
@@ -0,0 +1,199 @@
+
+#include "textures.h"
+
+#include  "img/h/0.h"
+#include  "img/h/1.h"
+#include  "img/h/2.h"
+#include  "img/h/3.h"
+#include  "img/h/4.h"
+#include  "img/h/5.h"
+#include  "img/h/6.h"
+#include  "img/h/7.h"
+#include  "img/h/8.h"
+#include  "img/h/9.h"
+#include "img/h/10.h"
+#include "img/h/11.h"
+#include "img/h/12.h"
+#include "img/h/13.h"
+#include "img/h/14.h"
+#include "img/h/15.h"
+#include "img/h/16.h"
+#include "img/h/17.h"
+#include "img/h/18.h"
+#include "img/h/19.h"
+#include "img/h/20.h"
+#include "img/h/21.h"
+#include "img/h/22.h"
+#include "img/h/23.h"
+#include "img/h/24.h"
+#include "img/h/25.h"
+#include "img/h/26.h"
+#include "img/h/27.h"
+#include "img/h/28.h"
+#include "img/h/29.h"
+#include "img/h/30.h"
+
+// static uint8_t * __alltex[31] = {
+// 	 __0_png,
+// 	 __1_png,
+// 	 __2_png,
+// 	 __3_png,
+// 	 __4_png,
+// 	 __5_png,
+// 	 __6_png,
+// 	 __7_png,
+// 	 __8_png,
+// 	 __9_png,
+// 	__10_png,
+// 	__11_png,
+// 	__12_png,
+// 	__13_png,
+// 	__14_png,
+// 	__15_png,
+// 	__16_png,
+// 	__17_png,
+// 	__18_png,
+// 	__19_png,
+// 	__20_png,
+// 	__21_png,
+// 	__22_png,
+// 	__23_png,
+// 	__24_png,
+// 	__25_png,
+// 	__26_png,
+// 	__27_png,
+// 	__28_png,
+// 	__29_png,
+// 	__30_png,
+// };
+
+// const uint8_t ** textures = __alltex;
+
+static png_bin_t __alltex[31] = {
+	(png_bin_t){
+		.data = __0_png,
+		.len  = __0_png_len,
+	},
+	(png_bin_t){
+		.data = __1_png,
+		.len  = __1_png_len,
+	},
+	(png_bin_t){
+		.data = __2_png,
+		.len  = __2_png_len,
+	},
+	(png_bin_t){
+		.data = __3_png,
+		.len  = __3_png_len,
+	},
+	(png_bin_t){
+		.data = __4_png,
+		.len  = __4_png_len,
+	},
+	(png_bin_t){
+		.data = __5_png,
+		.len  = __5_png_len,
+	},
+	(png_bin_t){
+		.data = __6_png,
+		.len  = __6_png_len,
+	},
+	(png_bin_t){
+		.data = __7_png,
+		.len  = __7_png_len,
+	},
+	(png_bin_t){
+		.data = __8_png,
+		.len  = __8_png_len,
+	},
+	(png_bin_t){
+		.data = __9_png,
+		.len  = __9_png_len,
+	},
+	(png_bin_t){
+		.data = __10_png,
+		.len  = __10_png_len,
+	},
+	(png_bin_t){
+		.data = __11_png,
+		.len  = __11_png_len,
+	},
+	(png_bin_t){
+		.data = __12_png,
+		.len  = __12_png_len,
+	},
+	(png_bin_t){
+		.data = __13_png,
+		.len  = __13_png_len,
+	},
+	(png_bin_t){
+		.data = __14_png,
+		.len  = __14_png_len,
+	},
+	(png_bin_t){
+		.data = __15_png,
+		.len  = __15_png_len,
+	},
+	(png_bin_t){
+		.data = __16_png,
+		.len  = __16_png_len,
+	},
+	(png_bin_t){
+		.data = __17_png,
+		.len  = __17_png_len,
+	},
+	(png_bin_t){
+		.data = __18_png,
+		.len  = __18_png_len,
+	},
+	(png_bin_t){
+		.data = __19_png,
+		.len  = __19_png_len,
+	},
+	(png_bin_t){
+		.data = __20_png,
+		.len  = __20_png_len,
+	},
+	(png_bin_t){
+		.data = __21_png,
+		.len  = __21_png_len,
+	},
+	(png_bin_t){
+		.data = __22_png,
+		.len  = __22_png_len,
+	},
+	(png_bin_t){
+		.data = __23_png,
+		.len  = __23_png_len,
+	},
+	(png_bin_t){
+		.data = __24_png,
+		.len  = __24_png_len,
+	},
+	(png_bin_t){
+		.data = __25_png,
+		.len  = __25_png_len,
+	},
+	(png_bin_t){
+		.data = __26_png,
+		.len  = __26_png_len,
+	},
+	(png_bin_t){
+		.data = __27_png,
+		.len  = __27_png_len,
+	},
+	(png_bin_t){
+		.data = __28_png,
+		.len  = __28_png_len,
+	},
+	(png_bin_t){
+		.data = __29_png,
+		.len  = __29_png_len,
+	},
+	(png_bin_t){
+		.data = __30_png,
+		.len  = __30_png_len,
+	}
+};
+
+const png_bin_t * textures = __alltex;
