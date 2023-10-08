00f2d5af7b1c4028a6f766e2d525d8a23a67086c
computermouth <computermouth@crunchbangplusplus.org>
2023-10-07 18:25:36 -0600

    new nails pickup

--- entity_pickup_nails.c
+++ entity_pickup_nails.c
@@ -13,6 +13,12 @@ void entity_pickup_nails_constructor(entity_t * e, vec3_t pos, uint8_t p1, uint8
     e->_init = (void (*)(void *, uint8_t, uint8_t))entity_pickup_nails_init;
     e->_pickup = (void (*)(void *))entity_pickup_nail_pickup;
     e->_init(e, p1, p2);
+
+    e->_texture = e->_params->entity_generic_params.ref_entt->tex_id;
+    vector * frames = e->_params->entity_generic_params.ref_entt->frames;
+    uint32_t * uframes = vector_begin(frames);
+    e->_model->frames = uframes;
+    e->_model->nv = e->_params->entity_generic_params.ref_entt->vert_len;
 }
 
 void entity_pickup_nails_init(entity_t * e, uint8_t p1, uint8_t p2) {
